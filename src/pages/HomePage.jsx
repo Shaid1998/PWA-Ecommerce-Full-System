4a86b4d2731c550230344871267495b3c9d6a45b
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-21 16:23:36 +0600

    Working On Collection of Collection component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import FeaturedProducts from '../components/Home/FeaturedProducts'
 import Categories from '../components/Home/Categories'
+import Collection from '../components/Home/Collection'
 
 
 
@@ -9,6 +10,7 @@ class HomePage extends Component {
     return (
       <Fragment>
         <FeaturedProducts />
+        <Collection />
         <Categories />
       </Fragment>
     )

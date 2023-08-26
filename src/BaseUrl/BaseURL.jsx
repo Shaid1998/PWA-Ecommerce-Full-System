7afe7e7f950935cc7d3722653494806537bfd227
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-26 17:48:17 +0600

    About Us Api

--- src/BaseUrl/BaseURL.jsx
+++ src/BaseUrl/BaseURL.jsx
@@ -4,7 +4,8 @@ class BaseURL {
     static BaseUrl ='http://127.0.0.1:8000/api'
 
     static VisitorDetails = this.BaseUrl+'/getvisitordetails'
-    static PostContact = this.BaseURL+"/postcontact"
+    static PostContact = this.BaseURL+'/postcontact'
+    static AboutUs = this.BaseURL+'/getallabout'
 }
 
 export default BaseURL

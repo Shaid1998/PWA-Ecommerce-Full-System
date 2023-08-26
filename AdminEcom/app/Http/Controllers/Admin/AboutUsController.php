1b5bc061602fc7a90f85ab1b65ac836c5e3e9929
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-26 17:31:57 +0600

    About Us Api

--- AdminEcom/app/Http/Controllers/Admin/AboutUsController.php
+++ AdminEcom/app/Http/Controllers/Admin/AboutUsController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\AboutUs;
 
 class AboutUsController extends Controller
 {
-    //
+    public function getAboutUsDetails(){
+        $result = AboutUs::get();
+        return $result;
+    }//End Method
 }
--- AdminEcom/routes/api.php
+++ AdminEcom/routes/api.php
@@ -4,24 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\ContactController;
+use App\Http\Controllers\Admin\AboutUsController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
- //   return $request->user();
-//});
 
 //Visitor Route
 Route::get('/getvisitordetails', [VisitorController::class, 'getVisitorDetails']);
 
 //Contact Route
-Route::get('/postcontact', [ContactController::class, 'PostContactDetails']);
+Route::post('/postcontact', [ContactController::class, 'PostContactDetails']);
+
+
+//Visitor Route
+Route::get('/getallabout', [AboutUsController::class, 'getAboutUsDetails']);
